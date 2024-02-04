6d9880d77ca59a15a7bc81fba5e83b302f75c685
MrMikki-boop <nickitkacozloff@yandex.ru>
2024-02-04 15:50:16 +0300

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
 	application
+	checkstyle
 	jacoco
 	java
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("io.freefair.lombok") version "8.4"
 
 	id("se.patrikerdes.use-latest-versions") version "0.2.18"
 	id("com.github.ben-manes.versions") version "0.41.0"
@@ -31,6 +33,7 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-devtools")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
@@ -39,4 +42,29 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+checkstyle {
+	toolVersion = "10.3.3"
+}
+
+tasks.bootBuildImage {
+	builder.set("paketobuildpacks/builder-jammy-base:latest")
+}
+
+tasks.test {
+	useJUnitPlatform()
+	finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.withType<Test> {
+	finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+	reports {
+		xml.required.set(true)
+		csv.required.set(true)
+		html.required.set(true)
+	}
 }
