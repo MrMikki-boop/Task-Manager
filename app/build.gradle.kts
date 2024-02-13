10c581d7a122dbdab43d9af64af6a2d89be14295
MrMikki-boop <nickitkacozloff@yandex.ru>
2024-02-13 21:14:09 +0300

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,19 +38,48 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter:3.2.2")
-	implementation("org.springframework.boot:spring-boot-starter-web:3.2.2")
-	implementation("org.springframework.boot:spring-boot-devtools:3.2.2")
+	// Spring Boot Starters
+	implementation("org.springframework.boot:spring-boot-starter:3.1.0")
+	implementation("org.springframework.boot:spring-boot-starter-web:3.1.0")
+	implementation("org.springframework.boot:spring-boot-devtools:3.0.4")
+
+	// Lombok for Compile-time
 	compileOnly("org.projectlombok:lombok:1.18.30")
 	annotationProcessor("org.projectlombok:lombok:1.18.30")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-validation:3.1.5")
-	runtimeOnly("com.h2database:h2:2.1.214")
-	implementation("org.postgresql:postgresql")
 
+	// MapStruct for Object Mapping
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+
+	// Spring Boot Data JPA
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.0.4")
+
+	// Spring Boot Validation
+	implementation("org.springframework.boot:spring-boot-starter-validation:3.1.5")
+
+	// H2 Database for Development
+	runtimeOnly("com.h2database:h2:2.1.214")
+
+	// Spring Boot Configuration Processor
+	implementation("org.springframework.boot:spring-boot-configuration-processor:3.1.2")
+
+	// Spring Boot Security
+	implementation("org.springframework.boot:spring-boot-starter-security:3.0.4")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server:3.1.0")
+
+	// Testing Dependencies
+	testImplementation(platform("org.junit:junit-bom:5.10.0"))
+	testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.0")
+	testImplementation("org.springframework.security:spring-security-test:6.0.2")
+	testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+	implementation("net.datafaker:datafaker:2.0.1")
+	implementation("org.instancio:instancio-junit:3.3.0")
+
+	// Test-only Lombok Dependencies
 	testCompileOnly("org.projectlombok:lombok:1.18.30")
 	testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
-	testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.0")
 }
 
 tasks.withType<Test> {
