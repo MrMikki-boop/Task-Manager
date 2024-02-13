b6799f60966b02d6db5579d35945651d193e1505
MrMikki-boop <nickitkacozloff@yandex.ru>
2024-02-13 21:17:19 +0300

    update UserRepository.java

--- app/src/main/java/hexlet/code/repository/UserRepository.java
+++ app/src/main/java/hexlet/code/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package hexlet.code.repository;
-
 import hexlet.code.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByFirstName(String firstName);
+    Optional<User> findByEmail(String email);
 }
