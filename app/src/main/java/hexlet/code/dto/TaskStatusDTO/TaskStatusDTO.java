101d8247131b32d70367173c2d7bce7d4547c318
MrMikki-boop <nickitkacozloff@yandex.ru>
2024-02-15 02:47:46 +0300

    update TaskStatusDTO package

--- app/src/main/java/hexlet/code/dto/TaskStatusDTO/TaskStatusDTO.java
+++ app/src/main/java/hexlet/code/dto/TaskStatusDTO/TaskStatusDTO.java
@@ -1,5 +1,6 @@
 package hexlet.code.dto.TaskStatusDTO;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +13,8 @@ public class TaskStatusDTO {
     private Long id;
     private String name;
     private String slug;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate createdAt;
 
 }
