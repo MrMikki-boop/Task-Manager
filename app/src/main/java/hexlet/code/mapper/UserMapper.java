17505c00417ccd098519a0bce756d5abda61d603
MrMikki-boop <nickitkacozloff@yandex.ru>
2024-02-15 00:58:25 +0300

    add mapper package

--- app/src/main/java/hexlet/code/mapper/UserMapper.java
+++ app/src/main/java/hexlet/code/mapper/UserMapper.java
@@ -1,15 +1,18 @@
 package hexlet.code.mapper;
 
-import hexlet.code.dto.UserCreateDTO;
-import hexlet.code.dto.UserDTO;
-import hexlet.code.dto.UserUpdateDTO;
+import hexlet.code.dto.UserDTO.UserCreateDTO;
+import hexlet.code.dto.UserDTO.UserDTO;
+import hexlet.code.dto.UserDTO.UserUpdateDTO;
 import hexlet.code.model.User;
+
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
-import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.BeforeMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Mapper(
         uses = {JsonNullableMapper.class, ReferenceMapper.class},
@@ -19,11 +22,18 @@ import org.mapstruct.ReportingPolicy;
 )
 public abstract class UserMapper {
 
-    @Mapping(target = "passwordDigest", source = "password")
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public abstract User map(UserCreateDTO dto);
 
-    public abstract UserDTO map(User model);
+    public abstract UserDTO map(User user);
 
-    @Mapping(target = "passwordDigest", source = "password")
-    public abstract void update(UserUpdateDTO dto, @MappingTarget User model);
+    public abstract void update(UserUpdateDTO data, @MappingTarget User model);
+
+    @BeforeMapping
+    public void encryptPassword(UserCreateDTO data) {
+        var password = data.getPassword();
+        data.setPassword(passwordEncoder.encode(password));
+    }
 }
