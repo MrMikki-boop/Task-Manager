ea6d9e32ad75ec94ccacf098a9dcfd94d6f6c5ef
MrMikki-boop <nickitkacozloff@yandex.ru>
2024-02-15 02:48:30 +0300

    add TaskDTO package. add TaskCreateDTO.java and TaskDTO.java and TaskParamsDTO.java and TaskUpdateDTO.java

---
+++ app/src/main/java/hexlet/code/dto/TaskDTO/TaskCreateDTO.java
@@ -0,0 +1,30 @@
+package hexlet.code.dto.TaskDTO;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class TaskCreateDTO {
+
+    private Long index;
+
+    @JsonProperty("assignee_id")
+    private Long assigneeId;
+
+    @NotBlank
+    private String title;
+
+    private String content;
+
+    @NotNull
+    private String status;
+
+    private List<Long> taskLabelIds;
+
+}
---
+++ app/src/main/java/hexlet/code/dto/TaskDTO/TaskDTO.java
@@ -0,0 +1,29 @@
+package hexlet.code.dto.TaskDTO;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+public class TaskDTO {
+
+    private Long id;
+    private Long index;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate createdAt;
+
+    @JsonProperty("assignee_id")
+    private Long assigneeId;
+
+    private String title;
+    private String content;
+    private String status;
+    private List<Long> taskLabelIds;
+
+}
