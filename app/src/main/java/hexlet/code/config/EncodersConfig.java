1847b96f657d33e8d83097dd6a25491b4b358995
MrMikki-boop <nickitkacozloff@yandex.ru>
2024-02-15 00:56:56 +0300

    update config package

--- app/src/main/java/hexlet/code/config/EncodersConfig.java
+++ app/src/main/java/hexlet/code/config/EncodersConfig.java
@@ -6,6 +6,7 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+
 import hexlet.code.component.RsaKeyProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -36,7 +37,7 @@ public class EncodersConfig {
      * @return Nimbus JWT-encoder with the given parameters
      */
     @Bean
-    JwtEncoder jwtEncoder() {
+    public JwtEncoder jwtEncoder() {
         JWK jwk = new RSAKey.Builder(rsaKeys.getPublicKey()).privateKey(rsaKeys.getPrivateKey()).build();
         JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
         return new NimbusJwtEncoder(jwks);
@@ -47,7 +48,7 @@ public class EncodersConfig {
      * @return Nimbus JWT-decoder with the given parameters
      */
     @Bean
-    JwtDecoder jwtDecoder() {
+    public JwtDecoder jwtDecoder() {
         return NimbusJwtDecoder.withPublicKey(rsaKeys.getPublicKey()).build();
     }
 }
